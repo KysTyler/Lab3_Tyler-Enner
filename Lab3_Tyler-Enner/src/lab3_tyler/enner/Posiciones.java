c66e9fd4348a96a57e26e2762f662542d1edf833
KysTyler <62519436+KysTyler@users.noreply.github.com>
2020-08-07 13:56:23 -0600

    Added Class Posiciones

---
+++ Lab3_Tyler-Enner/src/lab3_tyler/enner/Posiciones.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_tyler.enner;
+
+/**
+ *
+ * @author Tyler C
+ */
+public class Posiciones {
+    private String nombre;
+
+    public Posiciones() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Posiciones{" + "nombre=" + nombre + '}';
+    }
+    
+    
+}
