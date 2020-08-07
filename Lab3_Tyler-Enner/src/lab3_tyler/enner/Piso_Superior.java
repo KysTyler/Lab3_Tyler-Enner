6381068f631608f59f5115525df764f27a86cb2f
KysTyler <62519436+KysTyler@users.noreply.github.com>
2020-08-07 15:08:06 -0600

    Added Pisos and Subclasses

---
+++ Lab3_Tyler-Enner/src/lab3_tyler/enner/Piso_Superior.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_tyler.enner;
+
+/**
+ *
+ * @author Tyler C
+ */
+public class Piso_Superior extends Pisos{
+    
+}
--- Lab3_Tyler-Enner/src/lab3_tyler/enner/Pisos.java
+++ Lab3_Tyler-Enner/src/lab3_tyler/enner/Pisos.java
@@ -5,10 +5,73 @@
  */
 package lab3_tyler.enner;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ENNER
  */
 public class Pisos {
+    private String admin;
+    private Ranker evaluador;
+    private Ranker Director;
+    private ArrayList <Personas> persons = new ArrayList();
+    private int nivel;
+
+    public Pisos() {
+    }
+
+    public Pisos(String admin, Ranker evaluador, Ranker Director, int nivel) {
+        this.admin = admin;
+        this.evaluador = evaluador;
+        this.Director = Director;
+        this.nivel = nivel;
+    }
+
+    public String getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(String admin) {
+        this.admin = admin;
+    }
+
+    public Ranker getEvaluador() {
+        return evaluador;
+    }
+
+    public void setEvaluador(Ranker evaluador) {
+        this.evaluador = evaluador;
+    }
+
+    public Ranker getDirector() {
+        return Director;
+    }
+
+    public void setDirector(Ranker Director) {
+        this.Director = Director;
+    }
+
+    public ArrayList<Personas> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(ArrayList<Personas> persons) {
+        this.persons = persons;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    @Override
+    public String toString() {
+        return "Pisos{" + "admin=" + admin + ", evaluador=" + evaluador + ", Director=" + Director + ", persons=" + persons + ", nivel=" + nivel + '}';
+    }
+    
     
 }
